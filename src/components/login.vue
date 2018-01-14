1f1a4b3563ff219be8ad6c6e22398a68e59c9021
chiuwingyan <775039533@qq.com>
2018-01-14 22:42:46 +0800

    完善登录页面

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,16 +1,25 @@
 <template>
+
   <div class="login">
- 
-     <el-form ref="form" :model="form" label-width="80px" :rules="loginRules">
-  <el-form-item label="用户名" prop="name">
-    <el-input v-model="form.name" class="user-input"></el-input>
+
+     <el-form ref="form" :model="form"  :rules="loginRules" class="login-form">
+       <div class="title-container">
+        <h3 class="title">药房管理系统</h3>
+       
+      </div>
+  <el-form-item label="" prop="name">
+    <span class="svg-container svg-container_login">
+      <i class="iconfont icon-denglu"></i>
+    </span>
+    <el-input v-model="form.name" type="text"/>
   </el-form-item>
-   <el-form-item label="密码" prop="pass">
-    <el-input type="password" v-model="form.pass" auto-complete="off" class="user-pass"></el-input>
+   <el-form-item label="" prop="pass">
+     <span class="svg-container">
+      <i class="iconfont icon-denglu1"></i>
+    </span>
+    <el-input type="password" v-model="form.pass" auto-complete="off"/>
   </el-form-item>
-  <el-form-item size="large">
-    <el-button type="primary" @click="onSubmit">登录</el-button>
-  </el-form-item>
+ <el-button type="primary" style="width:100%;margin-bottom:30px;"  @click="onSubmit">登录</el-button>
   </el-form>
   </div>
 </template>
@@ -34,17 +43,103 @@ export default {
   methods:{
       onSubmit(){
         console.log(this.form.name,this.form.pass) 
+        this.$http.post('http://39.108.174.244:9090/oauth/login',{
+        "id": 0,
+        "isDisabled": 0,
+        "page": 0,
+        "password": this.form.pass,
+        "realName": "string",
+        "roleId": 0,
+        "rows": 0,
+        "username": this.form.name
+        }).then(function (response) {
+        console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+            }
+        }
       }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" type="text/css">    //重写element-ui
+
+$light_gray:#eee;
+$bg:#2d3a4b;
+.login{
+ width:100%;
+ height: 100%;
+ position: relative;
+ background-color:#2d3a4b;
+  .login-form{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    width: 520px;
+    padding: 35px 35px 15px 35px;
+    .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+  }
+  .title-container {
+    position: relative;
+    .title {
+      font-size: 26px;
+      font-weight: 400;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+  .el-input{
+        display: inline-block;
+        height: 47px;
+        width: 85%;
+        input{
+          background: transparent;
+          border: 0px;
+          -webkit-appearance: none;
+          border-radius: 0px;
+          padding: 12px 5px 12px 15px;
+          color: $light_gray;
+          height: 47px;
+          &:-webkit-autofill {
+            -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
+            -webkit-text-fill-color: #fff !important;
+          }
+        }
+    }
+  }
+}
+</style>
 <style lang="scss" scoped="" type="text/css">
-.user-input{
-  width: 204px;
-}
-.user-pass{
-  width:204px;
-}
+$bg:#2d3a4b;
+$dark_gray:#889aa4;
+$light_gray:#eee;
+.svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    width: 30px;
+    display: inline-block;
+    &_login {
+      font-size: 20px;
+    }
+  }
+  .el-form-item__content{
+    margin-left: 0;
+  }
+  .button{
+     position: absolute;
+    right: 35px;
+    bottom: 28px;
+  }
+
+    
 </style>

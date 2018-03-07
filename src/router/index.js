aa7c3e7128a47a10b7ec0024ce220b1f529b7c29
chiuwingyan <775039533@qq.com>
2018-03-07 14:36:49 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from 'components/login'
+import Login from '@/components/login'
 import Main from 'components/main'
 import Root from 'components/root/root'
 import Cashier from 'components/cashier/cashier'
